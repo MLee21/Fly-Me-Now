4337a58b893436449cd19e866995e3629232e1ba
Lori Culberson <bigskybandit@sbcglobal.net>
2015-08-01 02:19:16 -0600

    WIP Create forecast parser

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,20 +1,24 @@
 class WeatherService
-  attr_reader :connection
+  attr_reader :connection, :state, :city
 
-  def initialize
+  def initialize(state, city)
     @connection = Hurley::Client.new 'http://api.wunderground.com/'
-    # @connection = Hurley::Client.new "http://api.wunderground.com/api/#{ENV['weather_api_key']}"
+    @state = state
+    @city = city
   end
 
-  def forecast_data(state, city)
+  def forecast_data
     response = parse(connection.get("api/#{ENV['weather_api_key']}/forecast/q/#{state}/#{city}.json"))
-    response[:forecast][:simpleforecast][:forecastday]
+    weather_data = response[:forecast][:simpleforecast][:forecastday]
+    Forecast.new(weather_data)
   end
 
   private
   def parse(response)
     JSON.parse(response.body, symbolize_names: true)
   end
+
+
 
 end
 
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,20 +1,40 @@
 require "rails_helper"
 
 RSpec.describe 'WeatherService' do 
-  attr_reader :service 
+  attr_reader :service, :state, :city 
+  vcr_options = { allow_playback_repeats: true }
 
   before(:each) do 
-    @service = WeatherService.new
+    @state = "CO"
+    @city = "Denver"
+    @service = WeatherService.new(state, city)
   end
 
-  it "returns array of weather data" do
-    VCR.use_cassette("get_weather") do 
-      state = "CO"
-      city = "Denver"
+  describe "#forecast_data" do
+    it "returns a Forecast instance" do
+      VCR.use_cassette("get_weather", vcr_options) do 
+     
+        denver_weather = service.forecast_data
+        expect(denver_weather).to be_an_instance_of(Forecast)
+      end
+    end
 
-      denver_weather = service.forecast_data(state, city)
-      expect(denver_weather).to be_an_instance_of(Array)
+    it "calls Forecast.new with the weather data" do
+      url = "http://api.wunderground.com/api/#{ENV['weather_api_key']}/forecast/q/#{state}/#{city}.json"
+      weather_data = [ "3 days of weather data"]
+      stub_request(:get, url).
+        to_return(
+          body: JSON.generate(
+            forecast: {
+              simpleforecast: { forecastday: weather_data }
+            }
+          )
+        )
+
+      allow(Forecast).to receive(:new)
+
+      service.forecast_data
+      expect(Forecast).to have_received(:new).with(weather_data) 
     end
   end
-
 end
