e4ef6f1786f133702fb8a74b919d5f7b8571cdf5
MLee21 <minnie.lee89@gmail.com>
2015-08-03 13:39:20 -0600

    consumed flight api.

--- spec/services/flight_information_service_spec.rb
+++ spec/services/flight_information_service_spec.rb
@@ -9,9 +9,19 @@ RSpec.describe 'FlightInformationService', type: :model do
 
   it "returns flights" do
     VCR.use_cassette("get_flights") do 
-      location = "Denver, CO"
-      flights = service.flights_by_location(location)
-      expect(flights).to be_an_instance_of(Array)
+      location = "DEN"
+      price = 1000
+      flights = service.flights_by_location(location, price)
+      expect(flights).to be_an_instance_of(Hash)
+    end
+  end
+
+  xit "returns information for flights" do 
+    VCR.use_cassette("get_flights") do 
+      location = "DEN"
+      price = 1000
+      flights = service.flights_by_location(location, price)
+      expect(flights["Journeys"]).to eq("First Responders")
     end
   end
 
