869ece3956a837ae7696be27e68e552b5a4a7ca0
Lori Culberson <bigskybandit@sbcglobal.net>
2015-08-05 12:51:56 -0600

    WIP show page for deasl

--- spec/deals_view_spec.rb
+++ spec/deals_view_spec.rb
@@ -1,28 +1,28 @@
- RSpec.describe ViewTemplates do 
+ # RSpec.describe ViewTemplates do 
 
-   it('should redirect index.html to index.html#/phones', function() {
-      browser.get('app/index.html');
-      browser.getLocationAbsUrl().then(function(url) {
-          expect(url).toEqual('/phones');
-        });
-    });
+ #   it('should redirect index.html to index.html#/phones', function() {
+ #      browser.get('app/index.html');
+ #      browser.getLocationAbsUrl().then(function(url) {
+ #          expect(url).toEqual('/phones');
+ #        });
+ #    });
 
-    describe('Phone list view', function() {
-      beforeEach(function() {
-        browser.get('app/index.html#/phones');
-      });
-  ...
+ #    describe('Phone list view', function() {
+ #      beforeEach(function() {
+ #        browser.get('app/index.html#/phones');
+ #      });
+ #  ...
 
-    describe('Phone detail view', function() {
+ #    describe('Phone detail view', function() {
 
-      beforeEach(function() {
-        browser.get('app/index.html#/phones/nexus-s');
-      });
+ #      beforeEach(function() {
+ #        browser.get('app/index.html#/phones/nexus-s');
+ #      });
 
 
-      it('should display placeholder page with phoneId', function() {
-        expect(element(by.binding('phoneId')).getText()).toBe('nexus-s');
-      });
-    });
+ #      it('should display placeholder page with phoneId', function() {
+ #        expect(element(by.binding('phoneId')).getText()).toBe('nexus-s');
+ #      });
+ #    });
     
- end
+ # end
