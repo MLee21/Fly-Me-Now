2292b32fb35eadd9a2deb71a04f56dfaad5b4b32
Lori Culberson <bigskybandit@sbcglobal.net>
2015-08-08 15:10:55 -0600

    Add destinations to flight controller

--- app/assets/javascripts/angular/controllers/flights_controller.js
+++ app/assets/javascripts/angular/controllers/flights_controller.js
@@ -1,6 +1,135 @@
 app.controller('flightsController', ['$scope', 'flightInformation', function($scope, flightInformation){
-  flightInformation.then(function(response) {
-    $scope.response = response;
-    console.log(response);
-  });
+
+  var destinations = [
+    {
+      "city": "Austin",
+      "state": "TX",
+      "airport_code": "AUS"
+    },
+    {
+      "city": "Boston",
+      "state": "MA",
+      "airport_code": "BOS"
+    },
+    {
+      "city": "Charleston",
+      "state": "SC",
+      "airport_code": "CHS"
+    },
+    {
+      "city": "Chicago",
+      "state": "IL",
+      "airport_code": "CHI"
+    },
+    {
+      "city": "Honolulu",
+      "state": "HI",
+      "airport_code": "HNL"
+    },
+    {
+      "city": "Houston",
+      "state": "TX",
+      "airport_code": "HOU"
+    },
+    {
+      "city": "Las Vegas",
+      "state": "NV",
+      "airport_code": "LAS"
+    },
+    {
+      "city": "Los Angeles",
+      "state": "CA",
+      "airport_code": "LAX"
+    },
+    {
+      "city": "Miami",
+      "state": "FL",
+      "airport_code": "MIA"
+    },
+    {
+      "city": "Myrtle Beach",
+      "state": "SC",
+      "airport_code": "MYR"
+    },
+    {
+      "city": "Nashville",
+      "state": "TN",
+      "airport_code": "BNA"
+    },
+    {
+      "city": "New Orleans",
+      "state": "LA",
+      "airport_code": "MSY"
+    },
+    {
+      "city": 'New York',
+      "state": "NY",
+      "airport_code": "NYC",
+    },
+    {
+      "city": 'Orlando',
+      "state": "FL",
+      "airport_code": "MCO"
+    },
+    {
+      "city": "Palm Springs",
+      "state": "CA",
+      "airport_code": "PSP"
+    },
+    {
+      "city": "Phoenix",
+      "state": "AZ",
+      "airport_code": "PHX"
+    },
+    {
+      "city": "Portland",
+      "state": "OR",
+      "airport_code": "PDX"
+    },
+    {
+      "city": "San Antonio",
+      "state": "TX",
+      "airport_code": "SAT"
+    },
+    {
+      "city": "San Diego",
+      "state": "CA",
+      "airport_code": "SAN"
+    },
+    {
+      "city": "San Francisco",
+      "state":"CA",
+      "airport_code": "SFO"
+    },
+    {
+      "city": "Seattle",
+      "state": "WA",
+      "airport_code": "SEA"
+    },
+    {
+      "city": "Washington",
+      "state": "DC",
+      "airport_code": "WAS"
+    },
+    {
+      "city": "San Juan",
+      "state": "PR",
+      "airport_code": "SJU"
+    }
+  ]
+
+
+
+
+
+
+
+
+
+
+
+  // flightInformation.then(function(response) {
+  //   $scope.response = response;
+  //   console.log(response);
+  // });
 }]);
---
+++ app/model/travel_destinations.rb
@@ -0,0 +1,30 @@
+class TravelDestinations
+  def travel_destinations
+    { city: "Austin",
+      state: "TX",
+      airport:  "AUS"
+Boston  MA  BOS
+Denver  CO  DEN
+Charleston  SC  CHS
+Chicago IL  CHI
+Honolulu  HI  HNL
+Houston TX  HOU
+Las Vegas NV  LAS
+Los Angeles CA  LAX
+Miami FL  MIA
+Myrtle Beach  SC  MYR
+Nashville TN  BNA
+New Orleans LA  MSY
+New York  NY  NYC
+Orlando FL  MCO
+Palm Springs  CA  PSP
+Phoenix AZ  PHX
+Portland  OR  PDX
+San Antonio TX  SAT
+San Diego CA  SAN
+San Francisco CA  SFO
+Seattle WA  SEA
+Washington  DC  WAS
+San Juan  PR  SJU
+  end
+end
